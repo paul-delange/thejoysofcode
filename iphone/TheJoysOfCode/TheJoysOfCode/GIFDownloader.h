a83fd69461b1f5eb09a3c4db8ae2570e6f30f774
Paul de Lange <contact@talldevelopments.com>
2012-10-30 17:46:30 +0100

    gif decoder

---
+++ iphone/TheJoysOfCode/TheJoysOfCode/GIFDownloader.h
@@ -0,0 +1,13 @@
+//
+//  GIFDownloader.h
+//  TheJoysOfCode
+//
+//  Created by Bob on 29/10/12.
+//  Copyright (c) 2012 Tall Developments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GIFDownloader : NSObject
+
+@end
---
+++ iphone/TheJoysOfCode/TheJoysOfCode/GIFDownloader.m
@@ -0,0 +1,13 @@
+//
+//  GIFDownloader.m
+//  TheJoysOfCode
+//
+//  Created by Bob on 29/10/12.
+//  Copyright (c) 2012 Tall Developments. All rights reserved.
+//
+
+#import "GIFDownloader.h"
+
+@implementation GIFDownloader
+
+@end
